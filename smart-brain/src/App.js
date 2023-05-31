f528521f9e22d2ab1b3be7416a38b325cd833f44
Bobby <enomatebobby@gmail.com>
2023-05-31 13:31:04 +0100

    progress on the app

--- smart-brain/src/App.js
+++ smart-brain/src/App.js
@@ -1,111 +1,107 @@
 import React from 'react';
-import { useCallback } from "react";
-import Particles from "react-tsparticles";
-import { loadFull } from "tsparticles";
+import { Component } from 'react';
 import Navigation from './components/Navigation/Nav';
 import Logo from './components/Logo/Logo'
 import LinkForm from './components/linkForm/linkForm';
 import Rank from './components/Rank/rank';
+import Signin from './components/SignIn/SIgnIn';
+import Register from './components/Register/Register';
+import FaceRecognition from './components/faceRecognition/faceRecognition'
 import './App.css';
 
 
 
-function App() {
-  const particlesInit = useCallback(async engine => {
-    console.log(engine);
-    await loadFull(engine);
-}, []);
+// URL of image to use. Change this to your image.
 
-const particlesLoaded = useCallback(async container => {
-  await console.log(container);
-}, []);
+// NOTE: MODEL_VERSION_ID is optional, you can also call prediction with the MODEL_ID only
+// https://api.clarifai.com/v2/models/{YOUR_MODEL_ID}/outputs
+// this will default to the latest version_id
 
-const particlesOption = {
-  background: {
-      color: {
-          value: "#0d47a1",
-      },
-  },
-  fpsLimit: 120,
-  interactivity: {
-      events: {
-          onClick: {
-              enable: true,
-              mode: "push",
-          },
-          onHover: {
-              enable: true,
-              mode: "repulse",
-          },
-          resize: true,
-      },
-      modes: {
-          push: {
-              quantity: 4,
-          },
-          repulse: {
-              distance: 200,
-              duration: 0.4,
-          },
-      },
-  },
-  particles: {
-      color: {
-          value: "#ffffff",
-      },
-      links: {
-          color: "#ffffff",
-          distance: 150,
-          enable: true,
-          opacity: 0.5,
-          width: 1,
-      },
-      collisions: {
-          enable: true,
-      },
-      move: {
-          directions: "none",
-          enable: true,
-          outModes: {
-              default: "bounce",
-          },
-          random: false,
-          speed: 6,
-          straight: false,
-      },
-      number: {
-          density: {
-              enable: true,
-              area: 800,
-          },
-          value: 80,
-      },
-      opacity: {
-          value: 0.5,
-      },
-      shape: {
-          type: "circle",
-      },
-      size: {
-          value: { min: 1, max: 5 },
-      },
-  },
-  detectRetina: true,
-}
 
-  return (
-    <div className="App">
-      <Particles   id="tsparticles"
-            init={particlesInit}
-            loaded={particlesLoaded}
-            option = {{particlesOption}} />
-      <Navigation />
-      <Logo />
-      <Rank />
-     <LinkForm />
-      {/* { <FaceRecognition />} */}
-    </div>
-  );
+  
+
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imageUrl: '',
+      route: 'signin',
+      isSignedIn: false
+    }
+  }
+
+  onInputChange = (event) => {
+    this.setState({input: event.target.value});
+  }
+  onButtonSubmit = () => {
+    const IMAGE_URL = console.log(this.setState({ imageUrl: this.state.input })) ;
+
+    const raw = JSON.stringify({
+      "user_app_id": {
+        "user_id": "clarifai",
+        "app_id": "main"
+      },
+      "inputs": [
+          {
+              "data": {
+                  "image": {
+                      "url": IMAGE_URL
+                  }
+              }
+          }
+      ]
+    });
+
+    const requestOptions = {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Authorization': 'Key f8b2ccda7ca54a0886fc37bca359b849'
+        },
+        body: raw
+    };
+
+    fetch(`https://api.clarifai.com/v2/models/face-detection/versions/6dc7e46bc9124c5c8824be4822abe105/outputs`, requestOptions)
+    .then(response => response.text())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));
+  }
+
+  onRouteChange = (route) => {
+    if (route === 'signout' ){
+      this.setState({isSignedIn: false})
+    } else if (route === 'home'){
+      this.setState({isSignedIn: true})
+    }
+    this.setState( {route: route });
+  }
+
+  render () {
+    const { isSignedIn, imageUrl, route } = this.state;
+      return (
+        <div className="App">
+          <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange}/>
+          { route === 'home'
+            ? <div>
+                <Logo />
+                <Rank />
+                <LinkForm 
+                  onInputChange={this.onInputChange} 
+                  onButtonSubmit={this.onButtonSubmit}
+                />
+                <FaceRecognition imageUrl={imageUrl}/>
+              </div>
+            : (
+              this.state.route === 'signin' 
+              ? <Signin onRouteChange={this.onRouteChange}/>
+              : <Register  onRouteChange={this.onRouteChange}/>
+            )
+          }
+        </div>
+      );
+  }
 }
 
 export default App;
--- smart-brain/src/components/linkForm/linkForm.js
+++ smart-brain/src/components/linkForm/linkForm.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './linkForm.css';
-const LinkForm = () => {
+const LinkForm = ({ onInputChange, onButtonSubmit }) => {
   return (
    <div>
       <p className='f3'>
@@ -8,8 +8,8 @@ const LinkForm = () => {
       </p>
       <div className='center'>
         <div className='form center pa4 br3 shadow-5'>
-          <input className='f4 pa2 w-70 center' type='text' />
-          <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'>
+          <input className='f4 pa2 w-70 center' type='text' onChange={onInputChange}/>
+          <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple' onClick={onButtonSubmit}>
             Detect
           </button>
         </div>
